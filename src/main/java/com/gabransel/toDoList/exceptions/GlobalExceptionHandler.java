603fda162da2e34dcdd9b1575e9160160081edfd
Gabriel Anselmo <gabriel.anselmo.98@gmail.com>
2025-06-04 11:28:04 -0300

    Exceptions Anotations

--- src/main/java/com/gabransel/toDoList/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gabransel/toDoList/exceptions/GlobalExceptionHandler.java
@@ -2,17 +2,23 @@ package com.gabransel.toDoList.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(TaskLateException.class)
     public ResponseEntity<String> handleTaskLate(TaskLateException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Ocorreu um erro inesperado: " + ex.getMessage());
